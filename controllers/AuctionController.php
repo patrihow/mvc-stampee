b66fe9d36fa91895da9320f6c9e0655a51085a0e
Patricia <pbravo.art@gmail.com>
2025-04-09 15:10:38 -0400

    WIP function storeShow dans BidController

--- controllers/AuctionController.php
+++ controllers/AuctionController.php
@@ -3,10 +3,10 @@ namespace App\Controllers;
 
 use App\Models\Auction;
 use App\Models\Color;
+use App\Models\Country;
 use App\Models\ImageToUpload;
 use App\Models\Stamp;
 use App\Models\StampCondition;
-use App\Models\Country;
 use App\Models\Theme;
 use App\Providers\View;
 
@@ -22,26 +22,26 @@ class AuctionController
         $objAuction = new Auction;
         $auctions   = $objAuction->select();
 
-        $objStamp          = new Stamp;
-        $objImageToUpload  = new ImageToUpload;
-        $obtStampCondition = new StampCondition;
-        $objColor          = new Color;
-        $objCountry = new Country;
-        $objTheme = new Theme;
+        $stamp         = new Stamp;
+        $ImageToUpload = new ImageToUpload;
+        $conditions    = new StampCondition;
+        $objColor      = new Color;
+        $objCountry    = new Country;
+        $objTheme      = new Theme;
 
         $year = isset($_GET["year"]);
 
         foreach ($auctions as $auctionsIndex => $auction) {
 
-            $stampData = $objStamp->selectID(value: $auction['stamp_id']);
+            $stampData                             = $stamp->selectID(value: $auction['stamp_id']);
             $auctions[$auctionsIndex]['stampName'] = $stampData['name'];
             $auctions[$auctionsIndex]['yearStamp'] = $stampData['year'];
 
-            $stampConditionData = $obtStampCondition->selectID($stampData['stamp_condition_id']);
+            $stampConditionData = $conditions->selectID($stampData['stamp_condition_id']);
 
             $auctions[$auctionsIndex]['stamp_condition'] = $stampConditionData["name"];
 
-            $stampImages = $objImageToUpload->selectID($auction['stamp_id'], whereField: "stamp_id");
+            $stampImages = $ImageToUpload->selectID($auction['stamp_id'], whereField: "stamp_id");
 
             foreach ($stampImages as $stampImagesIndex => $image) {
                 if ($image["position"] == 0) {
--- controllers/BidController.php
+++ controllers/BidController.php
@@ -3,9 +3,12 @@ namespace App\Controllers;
 
 use App\Models\Auction;
 use App\Models\Bid;
+use App\Models\Color;
+use App\Models\Country;
 use App\Models\ImageToUpload;
 use App\Models\Stamp;
 use App\Models\StampCondition;
+use App\Models\Theme;
 use App\Models\User;
 use App\Providers\Validator;
 use App\Providers\View;
@@ -15,27 +18,28 @@ class BidController
     public function store($data = [])
     {
         $validator = new Validator;
-        $validator->field('bid', $data['bid'], "Bid input")->number();
+
+        $validator->field('bid_amount', $data['bid'], "Bid input")->number();
 
         if (! empty($_SESSION)) {
-            $objAuction        = new Auction;
-            $objStamp          = new Stamp;
-            $objImageToUpload  = new ImageToUpload;
-            $obtStampCondition = new StampCondition;
-            $objBid            = new Bid;
-            $objUser           = new User;
+            $auctionModel = new Auction;
+            $stamp        = new Stamp;
+            $imageModel   = new ImageToUpload;
+            $conditions   = new StampCondition;
+            $bidModel     = new Bid;
+            $user         = new User;
 
             if ($validator->isSuccess()) {
-                $auctionDetails = $objAuction->selectId($data["id"]);
-                $highestBid     = $objBid->selectWhereIdMax("auction_id", $data["id"], "bid");
+                $auctionDetails = $auctionModel->selectId($data["id"]);
+                $highestBid     = $bidModel->selectWhereIdMax("auction_id", $data["id"], "bid_amount");
 
-                if ($data["bid"] > $auctionDetails["starting_price"] && $data["bid"] > $highestBid["bid"]) {
+                if ($data["bid_amount"] > $auctionDetails["starting_price"] && $data["bid_amount"] > $highestBid["bid_amount"]) {
                     $dataInsert               = [];
-                    $dataInsert["bid"]        = $data["bid"];
+                    $dataInsert["bid_amount"] = $data["bid_amount"];
                     $dataInsert["auction_id"] = $data["id"];
                     $dataInsert["user_id"]    = $_SESSION['user_id'];
 
-                    $insertBid = $objBid->insert($dataInsert);
+                    $insertBid = $bidModel->insert($dataInsert);
                     if ($insertBid) {
                         return View::redirect('auction');
                     } else {
@@ -45,26 +49,26 @@ class BidController
                     return View::render('error', ['msg' => "Enchère trop basse"]);
                 }
             } else {
-                $auctions = $objAuction->select();
+                $auctions = $auctionModel->select();
 
                 foreach ($auctions as $auctionsIndex => $auction) {
-                    $stampData                                   = $objStamp->selectID($auction['stamp_id']);
+                    $stampData                                   = $stamp->selectID($auction['stamp_id']);
                     $auctions[$auctionsIndex]['stampName']       = $stampData['name'];
                     $auctions[$auctionsIndex]['yearStamp']       = $stampData['year'];
-                    $stampConditionData                          = $obtStampCondition->selectID($stampData['stamp_condition_id']);
+                    $stampConditionData                          = $conditions->selectID($stampData['stamp_condition_id']);
                     $auctions[$auctionsIndex]['stamp_condition'] = $stampConditionData["name"];
 
-                    $highestBid = $objBid->selectWhereIdMax("auction_id", $auction["id"], "bid");
+                    $highestBid = $bidModel->selectWhereIdMax("auction_id", $auction["id"], "bid_amount");
                     if ($highestBid && ! empty($highestBid)) {
-                        $auctions[$auctionsIndex]['highestBid']        = $highestBid["bid"];
-                        $highestBidderInfo                             = $objUser->selectIdWhere("id", $highestBid["user_id"]);
+                        $auctions[$auctionsIndex]['highestBid']        = $highestBid["bid_amount"];
+                        $highestBidderInfo                             = $user->selectIdWhere("id", $highestBid["user_id"]);
                         $auctions[$auctionsIndex]['highestBidderName'] = $highestBidderInfo["name"];
                     } else {
                         $auctions[$auctionsIndex]['highestBid']        = "Pas de mise";
                         $auctions[$auctionsIndex]['highestBidderName'] = "Aucun";
                     }
 
-                    $stampImages = $objImageToUpload->fetchAllById($auction['stamp_id'], "stamp_id");
+                    $stampImages = $imageModel->fetchAllById($auction['stamp_id'], "stamp_id");
                     foreach ($stampImages as $stampImagesIndex => $image) {
                         if ($image["position"] == 0) {
                             $auctions[$auctionsIndex]['file_name']   = $image['file_name'];
@@ -84,6 +88,38 @@ class BidController
     public function storeShow($data = [])
     {
         $validator = new Validator;
-        $validator->field('bid', $data['bid'], "input bid")->number(); // TODO: change to French! -- P'tit break :) I'm here
+
+        $validator->field('bid_amount', $data['bid'], "input bid")->number();
+
+        if (! empty($_SESSION)) {
+            $auctionModel  = new Auction;
+            $stamp         = new Stamp;
+            $ImageToUpload = new ImageToUpload;
+
+            $bidModel = new Bid;
+            $user     = new User;
+
+            $color      = new Color;
+            $conditions = new StampCondition;
+            $country    = new Country;
+            $theme      = new Theme;
+
+            $get       = ! empty($get) ? $get : $_GET;
+            $idAuction = $get["id"];
+
+            if ($validator->isSuccess()) {
+                $auctionDetails = $auctionModel->selectId($data["id"]);
+                $highestBid     = $bidModel->selectWhereIdMax("auction_id", $data["id"], "bid_amount");
+
+                // -- Voy aqui hay que hacer la condition if
+
+            }
+
+        }
     }
 }
+
+// echo('<pre>');
+// print_r($_FILES);
+// echo('</pre>');
+// die();
